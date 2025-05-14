0cca94e42ee073cba18952717557828825165e13
kakimaniSathish <sathish.kakimani@gmail.com>
2025-05-14 22:34:52 +0530

    TGV_123: created a test class

---
+++ TestClass.java
@@ -0,0 +1,9 @@
+public class TestClass {
+    private  int i = 10;
+
+    public static void main(String[] args) {
+        TestClass t = new TestClass();
+        int j = t.i*10;
+        System.out.println(j);
+    }
+}
